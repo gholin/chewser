0b40e435e2299d3bfa64ba0171703cf6605e5558
Steven Shepherd <sshepherd@instructure.com>
2016-04-11 15:33:38 -0600

    Second Commit
    Chewser has basic interface with some working elements. Can Create a new restaurant and see all your restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,33 @@
 class RestaurantsController < ApplicationController
+  #Lists all Restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # Shows the current restaurant
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Displays the new restaurant form page
+  def new
+  end
+
+  # Handles Saving a new restaurant. Redirects the page to the restaurant's show page
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  private
+
+  # Strong Params. We could just grab them all with params[:restaurant] but that would
+  # open it up to malicious acts. So, we make sure only to grab the ones that matter, in
+  # this case, :name
+  # Making it private further encapsulates and protects the method from outside influence
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
 end
